4d4167053c6a7504b8fafb6715eb5e21560a97c4
Mahmoud Shaheen <mahmoud_shaheen@live.com>
2018-06-09 02:50:45 +0200

    some network changes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var Turn = require('node-turn');
+const PORT = process.env.PORT || 3478;
+console.log(PORT);
+console.log("hello");
 var server = new Turn({
   // set options
-  authMech: 'long-term',
-  credentials: {
-    username: "password"
-  }
+  authMech: 'none',
+  debugLevel: 'ALL',
+  listeningIps: ['0.0.0.0']
+  listeningPort: `${PORT}`
 });
 server.start();
